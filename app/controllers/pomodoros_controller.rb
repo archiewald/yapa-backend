d3c2e630aa0f8063581848188cc030dfff635c86
archiewald <artur.kozubek1@gmail.com>
2019-09-10 22:04:21 +0200

    add create

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -1,7 +1,18 @@
 class PomodorosController < ApplicationController
   def index
     @pomodoros = Pomodoro.all
-
     render(json: @pomodoros, status: :ok)
+  end
+
+  def create
+    @pomodoro = Pomodoro.new(pomodoro_params)
+    @pomodoro.save
+    render(json: @pomodoro, status: :created)
+  end
+
+  private
+
+  def pomodoro_params
+    params.require(:pomodoro).permit(:name, :duration, :started_at)
   end
 end
