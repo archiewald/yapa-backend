845a9deee9968d63018d12419a947524c76cf7a2
archiewald <artur.kozubek1@gmail.com>
2019-09-14 14:48:51 +0200

    correct schema

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_072023) do
     t.datetime "started_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
