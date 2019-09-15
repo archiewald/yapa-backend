a16f827a0c10cb19ba98eb67751cb0cf2036d8e0
archiewald <artur.kozubek1@gmail.com>
2019-09-15 13:42:49 +0200

    add confirmation token to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_160112) do
+ActiveRecord::Schema.define(version: 2019_09_15_113019) do
 
   create_table "pomodoros", force: :cascade do |t|
     t.string "name"
@@ -31,7 +31,11 @@ ActiveRecord::Schema.define(version: 2019_09_14_160112) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "authentication_token", limit: 30
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
