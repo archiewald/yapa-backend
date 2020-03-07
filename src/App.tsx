171872d256c01bbadc964e765f2708e63de7b381
Artur Kozubek <artur.kozubek1@gmail.com>
2020-03-07 21:09:15 +0100

    reinitialize with CRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App: React.FC = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
