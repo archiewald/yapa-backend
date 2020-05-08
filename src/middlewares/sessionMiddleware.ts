5f9164b7716b38acaad36529810716706649f833
Artur Kozubek <artur.kozubek1@gmail.com>
2020-05-08 20:48:57 +0200

    make cookies secure

--- src/middlewares/sessionMiddleware.ts
+++ src/middlewares/sessionMiddleware.ts
@@ -6,14 +6,15 @@ const MongoStore = connectMongo(session);
 export function sessionMiddleware() {
   return session({
     store: new MongoStore({
-      url: process.env.MONGO_URL!
+      url: process.env.MONGO_URL!,
     }),
     secret: "secret",
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 30
-      //   secure: true
-    }
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      sameSite: "none",
+    },
   });
 }
