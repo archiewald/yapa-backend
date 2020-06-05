64869f93922ec6913faa24ca8f7990986c879889
Artur Kozubek <artur.kozubek1@gmail.com>
2020-06-05 15:27:22 +0200

    fix tags validation

--- src/pomodoros/validation.ts
+++ src/pomodoros/validation.ts
@@ -3,5 +3,5 @@ import * as Yup from "yup";
 export const createPomodoroValidationSchema = Yup.object({
   startDate: Yup.date().required(),
   duration: Yup.number().positive().integer().required(),
-  tags: Yup.array(Yup.string()).min(1),
+  tags: Yup.array(Yup.string()),
 });
